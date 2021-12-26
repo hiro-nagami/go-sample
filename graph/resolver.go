016acfff6b06dd292d27e7f699709c6b3adbec1f
hiro-nagami <hiro-nagami@users.noreply.github.com>
2021-12-26 12:15:58 +0900

    Add todo sample of todo

---
+++ graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+import (
+	"github.com/hiro-nagami/sample-go/graph/model"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
